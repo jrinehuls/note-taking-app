f1b861720a23c8d484f3fbb03a4deb2581303a11
Justin Rinehuls <jrinehuls@gmail.com>
2023-12-05 22:18:29 -0500

    map the notes

---
+++ src/collections/notes.js
@@ -0,0 +1,19 @@
+const notes = [
+    {
+        id: 1,
+        title: "This is the note title",
+        content: "This is the note content"
+    },
+    {
+        id: 2,
+        title: "This is another note title",
+        content: "This is another note content"
+    },
+    {
+        id: 3,
+        title: "This is yet another note title",
+        content: "This is yet another note content"
+    }
+]
+
+export default notes;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,23 @@
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../collections/notes"
+
+function renderNote(note) {
+    return (
+      <Note 
+        key={note.id}
+        title={note.title}
+        content={note.content}
+      />
+    );
+}
 
 function App() {
     return(
       <>
         <Header />
-        <Note title="This is the note title" content="This is the note content"/>
+        {notes.map(renderNote)}
         <Footer />
       </>
     );
