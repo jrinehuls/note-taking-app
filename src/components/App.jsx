9dd96b6e43722f7b073bd2706e46ae01c8ac2e1d
Justin Rinehuls <jrinehuls@gmail.com>
2023-12-06 21:54:51 -0500

    map notes with arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,24 +4,20 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../collections/notes"
 
-function renderNote(note) {
-    return (
-      <Note 
-        key={note.id}
-        title={note.title}
-        content={note.content}
-      />
-    );
-}
-
 function App() {
-    return(
-      <>
-        <Header />
-        {notes.map(renderNote)}
-        <Footer />
-      </>
-    );
+  return(
+    <>
+      <Header />
+      {notes.map(note => 
+        <Note 
+          key={note.id}
+          title={note.title}
+          content={note.content}
+        />
+      )}
+      <Footer />
+    </>
+  );
 }
 
 export default App
